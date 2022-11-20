4f3809f29eef760d453b9acf21b6cdb627440494
Daniel Lee <daniel@Daniels-Mac-mini.local>
2022-11-20 15:21:06 -0800

    fix for merge conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -16,33 +15,6 @@ export default function Home() {
       {
         writeToggle ? <Write /> : <Read />
       }
-=======
-import { useState } from "react"
-
-export default function Home() {
-	const [message,setMessage] = useState("")
-
-	const upload = () => {
-		const blob = new Blob(
-			["This is some asdfasd fa simportant text"],
-			{ type: "text/plain" }
-		)
-	
-		fetch('/api/upload', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'text/plain',
-			},
-			body: blob,
-		}).then((res) => res.json())
-		.then(data => setMessage(data.message))
-	}
-
-  return (
-    <div>
-      <button onClick={upload}>Submit</button>
-			<div>{message}</div>
->>>>>>> refs/remotes/origin/main
     </div>
   )
 }
